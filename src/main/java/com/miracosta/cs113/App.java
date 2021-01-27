496900a968fa239c60189d4eedd01bd0c04a9471
Erich Gutierrez <gutierrezerich3@gmail.com>
2021-01-26 20:21:51 -0800

    Initial lab project cs113

---
+++ src/main/java/com/miracosta/cs113/App.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.miracosta.cs113;
+
+/**
+ * @author erich
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//System.out.println("main");
+	}
+
+}
---
+++ src/main/java/com/miracosta/cs113/lab1/Lab1.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.miracosta.cs113.lab1;
+
+/**
+ * @author erich
+ *
+ */
+public class Lab1 {
+	
+	
+
+}
